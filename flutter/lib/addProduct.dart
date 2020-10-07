40afd4714dea94e8492971c4fbfa81ef0d5c2263
Jakub Karp <karptutaj@gmail.com>
2020-10-07 21:43:44 +0200

    dodałem dodawaczke

--- flutter/lib/addProduct.dart
+++ flutter/lib/addProduct.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class Add extends StatefulWidget {
   @override
@@ -7,42 +8,117 @@ class Add extends StatefulWidget {
 }
 
 class _AddState extends State<Add> {
+  Map _compositionElement = {
+    "id": 0,
+    "name": "",
+    "diffrentName": "",
+    "amount": 0
+  };
+
   Map product = {
     "brand": "",
-    "producent": "",
+    "name": "",
     "price": 0,
     "barcode": 0,
     "composition": []
   };
+
+  int _compositorCounter = 1;
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final _barcodeController = TextEditingController();
 
   Widget buildBrand() {
     return ListTile(
-      title: TextFormField(
-        validator: (String value) {
-          if (value.isEmpty) return "To pole nie może być puste";
-        },
-        onSaved: (String value) {
-          product["brand"] = value;
-        },
-      ),
-    );
+        title: TextFormField(
+      decoration: InputDecoration(hintText: "Marka"),
+      validator: (String value) {
+        if (value.isEmpty) return "To pole nie może być puste";
+      },
+      onSaved: (String value) {
+        product["brand"] = value;
+      },
+      textInputAction: TextInputAction.next,
+    ));
   }
 
-  Widget buildProducer() {
-    return null;
+  Widget buildName() {
+    return ListTile(
+        title: TextFormField(
+      decoration: InputDecoration(hintText: "Nazwa"),
+      validator: (String value) {
+        if (value.isEmpty) return "To pole nie może być puste";
+      },
+      onSaved: (String value) {
+        product["name"] = value;
+      },
+      textInputAction: TextInputAction.next,
+    ));
   }
 
   Widget buildPrice() {
-    return null;
+    return ListTile(
+        title: TextFormField(
+            decoration: InputDecoration(hintText: "Orientacyjna cena"),
+            keyboardType: TextInputType.number,
+            validator: (String value) {
+              if (value.isEmpty) return "To pole nie może być puste";
+            },
+            onSaved: (String value) {
+              product["price"] = value;
+            }));
   }
 
   Widget buildBarcode() {
-    return null;
+    return ListTile(
+        title: TextFormField(
+            controller: _barcodeController,
+            decoration: InputDecoration(
+                hintText: "Kod kreskowy",
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.scanner),
+                  onPressed: () async {
+                    await FlutterBarcodeScanner.scanBarcode(
+                            "#4caf50", "Anuluj", true, ScanMode.BARCODE)
+                        .then((value) => _barcodeController.text = value);
+                  },
+                )),
+            keyboardType: TextInputType.number,
+            validator: (String value) {
+              if (value.isEmpty) return "To pole nie może być puste";
+            },
+            onSaved: (String value) {
+              product["barcode"] = value;
+            }));
   }
 
-  Widget buildCompositor() {
-    return null;
+  Widget buildComposition() {
+    return Column(children: [
+      ListView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: _compositorCounter,
+        itemBuilder: (BuildContext ctxt, int index) {
+          product["composition"].addAll(_compositionElement);
+          return ListTile(
+            title: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Nazwa składnika"),
+                  validator: (String value) {
+                    if (value.isEmpty) return "To pole nie może być puste";
+                  },
+                  onSaved: (String value) {
+                    product["composition"][index]["name"] = value;
+                  },
+                  textInputAction: TextInputAction.next,
+                )
+              ],
+            ),
+          );
+        },
+      )
+    ]);
   }
 
   @override
@@ -57,6 +133,11 @@ class _AddState extends State<Add> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 buildBrand(),
+                buildBrand(),
+                buildName(),
+                buildPrice(),
+                buildBarcode(),
+                buildComposition(),
                 SizedBox(
                   height: 100,
                 ),
