721eb856dbd9a9da0d49d7499be141a46ef17f5b
kamciox09 <kamiliked11@gmail.com>
2020-10-25 15:00:41 +0100

    Added pagination

--- webapp/resources/views/pagination.blade.php
+++ webapp/resources/views/pagination.blade.php
@@ -1,3 +1,25 @@
-<article class="ShowMore">
-    <a href="{{ $paginator->nextPageUrl() }}">Pokaż więcej</a>
-</article>
+@if ($paginator->hasPages())
+    <nav role="navigation" aria-label="Pagination Navigation">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <span>
+                {!! __('pagination.previous') !!}
+            </span>
+        @else
+            <a href="{{ $paginator->previousPageUrl() }}" rel="prev">
+                {!! __('pagination.previous') !!}
+            </a>
+        @endif
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a href="{{ $paginator->nextPageUrl() }}" rel="next">
+                {!! __('pagination.next') !!}
+            </a>
+        @else
+            <span>
+                {!! __('pagination.next') !!}
+            </span>
+        @endif
+    </nav>
+@endif
